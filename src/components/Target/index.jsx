1440213e6e42aff83b1b1cb708ef7b7bb5913403
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-06 09:44:12 +0200

    Add target

---
+++ src/components/Target/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Target() {
+  return (
+    <div
+      style={{
+        width: "120px",
+        height: "120px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Grab me!
+    </div>
+  );
+}
+
+export default Target;
